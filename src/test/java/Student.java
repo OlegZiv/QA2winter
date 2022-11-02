fd7a74e3ca50981b3705aa0ffaf297a714afcfdb
OZ <test@test.lv>
2022-11-02 21:22:08 +0200

    Constructors / Loops / Java List

---
+++ src/test/java/Student.java
@@ -0,0 +1,50 @@
+public class Student {
+    private String firsName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Student() {
+
+    }
+
+    public Student(String firsName, String lastName, String email, String phone) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
