58d13b6c079689c71026bd03fedbb4230249441d
OZ <test@test.lv>
2022-11-05 20:21:20 +0200

    Find locators.

---
+++ src/test/java/LocatorsForPage.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class LocatorsForPage {
+    private final By RETURN_CARS = By.id(".//label[@for = 'is-drop-off']");
+    private final By IMG_DISCOVER_CAR = By.xpath(".//a[@class = 'navigation-link action my-booking trigger-login-modal pointer'] ");
+    private final By HOME_CAPTION = By.xpath(".//div[@class = 'home-caption'] ");
+    private final By CHOICE_COUNTRY = By.className(".//span[@class = 'sb-residence-text'] ");
+    private final By LOGIN_BTN = By.linkText(".//a[@class = 'navigation-link action my-booking trigger-login-modal pointer']");
+
+
+
+
+}
